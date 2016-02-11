76355f8c4b6c751aa2f1c0513fb8c62950a949a5
pinguo-xudianyang <xudianyang@camera360.com>
2016-02-11 10:41:27 +0800

    * 调整登录、logo等等

--- conf/users.auth.php
+++ conf/users.auth.php
@@ -9,4 +9,4 @@
 # login:passwordhash:Real Name:email:groups,comma,seperated
 
 
-xudianyang:$1$V6LxUoC0$Gqr2J93.82nOJiQD6YUw9.:xudianyang:xudianyang@camera360.com:admin,user
+xudianyang:$1$MdjjC9bN$hI9FppODYMFYvmVB4akT7/:xudianyang:120343758@qq.com:admin,user
